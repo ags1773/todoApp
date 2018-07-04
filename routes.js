1bbe193f77a378bd70b8860699bf2abd3e719a40
ags1773 <amogh1773@gmail.com>
2018-07-04 07:57:57 +0530

    Styled homepage, it now displays all todos

--- routes.js
+++ routes.js
@@ -2,10 +2,8 @@ const express = require('express')
 const router = express.Router()
 const storageArr = require('./storageArr')
 
-router.get('/', function (req, res) { // home page displays only titles of todo lists
-  let titles = []
-  storageArr.forEach(e => titles.push(e.title))
-  res.render('home', {titles: titles})
+router.get('/', function (req, res) {
+  res.render('home', {toDos: storageArr})
 })
 
 module.exports = router
--- storageArr.js
+++ storageArr.js
@@ -8,6 +8,16 @@ const storage = [
     title: 'To-Do 2',
     content: ['Get groceries', 'Repair generator', 'Clean house', 'Pay telephone bill'],
     id: 2
+  },
+  {
+    title: 'To-Do 3',
+    content: ['Get groceries', 'Repair generator', 'Clean house', 'Pay telephone bill'],
+    id: 3
+  },
+  {
+    title: 'To-Do 4',
+    content: ['Gfdsdh', 'Rdsfhdfar', 'Cadfade', 'Phfjkdl'],
+    id: 3
   }
 ]
 
